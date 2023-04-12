c1a0094ae2aeb4fe5e54fade84ce7bb843f44b8a
NSJoker <laughofnsjoker@gmail.com>
2023-04-12 08:54:22 +0530

    Initial Commit

---
+++ DeepLinks/DeepLinksApp.swift
@@ -0,0 +1,17 @@
+//
+//  DeepLinksApp.swift
+//  DeepLinks
+//
+//  Created by Chandrachudh K on 12/04/23.
+//
+
+import SwiftUI
+
+@main
+struct DeepLinksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
